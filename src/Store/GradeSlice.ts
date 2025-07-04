c678641f51a42d6fe4a856e3a92c06670e27d1c3
Smedge-Main <firosh.work760@gmail.com>
2025-07-04 15:14:51 +0530

    commit

--- src/Store/GradeSlice.ts
+++ src/Store/GradeSlice.ts
@@ -1,5 +1,5 @@
 // src/store/gradeSlice.ts
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 
 type Category = {
   name: string;
